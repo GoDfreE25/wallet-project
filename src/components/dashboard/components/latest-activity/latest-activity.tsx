d6563715cdf58f36147a8a1a9b38d1b03ba32cfd
Roman Khemych <roman19940912@gmail.com>
2025-01-15 16:25:35 +0200

    Add pagination to table and some UI fix

--- src/components/dashboard/components/latest-activity/latest-activity.tsx
+++ src/components/dashboard/components/latest-activity/latest-activity.tsx
@@ -30,7 +30,7 @@ const columns: TableProps<DataType>['columns'] = [
     key: 'status',
     dataIndex: 'status',
     render: (_, { status }) => (
-      <Tag color={ status === 'Rejected'? 'red' : 'green'} key={_}>
+      <Tag color={status === 'Rejected' ? 'red' : 'green'} key={_}>
         {status.toUpperCase()}
       </Tag>
     ),
@@ -41,12 +41,21 @@ export const LatestActivity = () => {
   const activities: DataType[] = [
     { id: 1, date: '10 Jan, 2024', description: 'Bye phone', amount: '100.00 USD', status: 'Confirmed' },
     { id: 2, date: '10 Jan, 2024', description: 'Send to Jhon', amount: '140.00 USD', status: 'Rejected' },
-    { id: 4, date: '10 Jan, 2024', description: 'Send to Mary', amount: '100.00 USD', status: 'Confirmed' },
     { id: 3, date: '10 Jan, 2024', description: 'Withdraw', amount: '100.00 EUR', status: 'Rejected' },
+    { id: 4, date: '10 Jan, 2024', description: 'Send to Mary', amount: '100.00 USD', status: 'Confirmed' },
+    { id: 5, date: '10 Jan, 2024', description: 'Withdraw', amount: '150.00 USD', status: 'Confirmed' },
   ];
   return (
     <div className={s.wrapper}>
-      <Table<DataType> columns={columns} dataSource={activities} />
+      <h3>Latest Activity</h3>
+      <Table<DataType>
+        columns={columns}
+        dataSource={activities}
+        pagination={{
+          defaultPageSize: 4,
+          locale: { items_per_page: 'page' },
+        }}
+      />
     </div>
   );
 };
